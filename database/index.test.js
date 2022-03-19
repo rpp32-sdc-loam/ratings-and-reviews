8f8aafe69998ed4783a7ac31a085f579fec7a877
Wilson Lessley <wilsonlessley14@gmail.com>
2022-03-19 10:02:02 -0700

    loaderio file wasnt staged for commit... now it is

---
+++ database/index.test.js
@@ -0,0 +1,24 @@
+var db = require('./index.js');
+
+xdescribe('Getting reviews', () => { //getProductReviews
+  test('should return reviews for the provided product ID', () => {
+
+  })
+});
+
+/*describe('Getting review meta data', () => { //getProductReviewMetadata
+
+});
+
+/*describe('Posting a new review', () => { //postReview
+
+});
+
+/*describe('Marking a review as helpful', () => { //markReviewHelpful
+
+});
+
+/*describe('Reporting a review', () => { //reportReview
+
+});
+/** */
